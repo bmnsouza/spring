0ec14ff6502a525daece7007bc0a84cca6ac0f20
Bruno Marcel <bmnsouza@gmail.com>
2021-09-17 23:02:52 -0300

    Atualização de pacote

--- spring-boot-admin-server/src/main/java/br/com/spring/boot/admin/SpringBootAdminServerApplication.java
+++ spring-boot-admin-server/src/main/java/br/com/spring/boot/admin/SpringBootAdminServerApplication.java
@@ -1,4 +1,4 @@
-package br.gov.se.sefaz;
+package br.com.spring.boot.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
