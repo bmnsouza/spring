20005eea542524ab905321fced3624efcfa88a5b
Bruno Marcel <bmnsouza@gmail.com>
2021-10-13 18:02:41 -0300

    Melhorias na classe CacheUtil

--- base-spring-boot/src/main/java/br/com/boot/spring/base/util/CacheUtil.java
+++ base-spring-boot/src/main/java/br/com/boot/spring/base/util/CacheUtil.java
@@ -9,17 +9,17 @@ import org.springframework.cache.annotation.Cacheable;
 
 public class CacheUtil {
 	
-	@Cacheable(value = "cache", key = "{#cdSistema, #nmSecao, #nmParametro, #vlParametro}")
-	public Map<String, Object> getFromCache(String cdSistema, String nmSecao, String nmParametro, String vlParametro) {
+	@Cacheable(value = "cache", key = "{#sistema, #parametro}")
+	public Map<String, Object> getFromCache(String sistema, String parametro) {
 	    throw new ServiceException("Objeto não encontrado");
 	}
 	
-	@CachePut(value = "cache", key = "{#cdSistema, #nmSecao, #nmParametro, #vlParametro}", unless = "#map == null")
-	public Map<String, Object> populateCache(String cdSistema, String nmSecao, String nmParametro, String vlParametro, Map<String, Object> map) {
+	@CachePut(value = "cache", key = "{#sistema, #parametro}", unless = "#map == null")
+	public Map<String, Object> populateCache(String sistema, String parametro, Map<String, Object> map) {
 		return map;
 	}
 	
-	@CacheEvict(value = "cache", key = "{#cdSistema, #nmSecao, #nmParametro, #vlParametro}")
-	public void forgetAboutThis(String cdSistema, String nmSecao, String nmParametro, String vlParametro) {}
+	@CacheEvict(value = "cache", key = "{#sistema, #parametro}")
+	public void forgetAboutThis(String sistema, String parametro) {}
 	
 }
