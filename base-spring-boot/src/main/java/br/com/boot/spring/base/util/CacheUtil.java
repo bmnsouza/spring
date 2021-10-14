1b90cb92a6edd1df149e46de10be1c247f12b12e
Bruno Marcel <bmnsouza@gmail.com>
2021-10-14 20:31:02 -0300

    Ajustes da classe CacheUtil

--- base-spring-boot/src/main/java/br/com/boot/spring/base/util/CacheUtil.java
+++ base-spring-boot/src/main/java/br/com/boot/spring/base/util/CacheUtil.java
@@ -1,25 +1,22 @@
 package br.com.boot.spring.base.util;
 
-import java.util.Map;
-
-import org.hibernate.service.spi.ServiceException;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 
 public class CacheUtil {
 	
-	@Cacheable(value = "cache", key = "{#sistema, #parametro}")
-	public Map<String, Object> getFromCache(String sistema, String parametro) {
-	    throw new ServiceException("Objeto não encontrado");
+	@Cacheable(value = "cache", key = "{#cdSistema, #nmSecao, #nmParametro}", unless = "#result == null")
+	public String getCache(String cdSistema, String nmSecao, String nmParametro) {
+		return null;
 	}
 	
-	@CachePut(value = "cache", key = "{#sistema, #parametro}", unless = "#map == null")
-	public Map<String, Object> populateCache(String sistema, String parametro, Map<String, Object> map) {
-		return map;
+	@CachePut(value = "cache", key = "{#cdSistema, #nmSecao, #nmParametro}", unless = "#vlParametro == null")
+	public String setCache(String cdSistema, String nmSecao, String nmParametro, String vlParametro) {
+		return vlParametro;
 	}
 	
-	@CacheEvict(value = "cache", key = "{#sistema, #parametro}")
-	public void forgetAboutThis(String sistema, String parametro) {}
-	
+	@CacheEvict(value = "cache", key = "{#cdSistema, #nmSecao, #nmParametro}")
+	public void delCache(String cdSistema, String nmSecao, String nmParametro) { }
+
 }
