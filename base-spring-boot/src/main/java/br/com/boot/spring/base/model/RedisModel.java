629352002708b6b0be8cac54d6953250c2f122a9
Bruno Marcel <bmnsouza@gmail.com>
2021-10-14 20:32:28 -0300

    Criação da classe RedisModel

---
+++ base-spring-boot/src/main/java/br/com/boot/spring/base/model/RedisModel.java
@@ -0,0 +1,16 @@
+package br.com.boot.spring.base.model;
+
+import lombok.Getter;
+
+@Getter
+public class RedisModel {
+	
+	private String cdSistema;
+	
+	private String nmSecao;
+	
+	private String nmParametro;	
+	
+	private String vlParametro;
+
+}
