057d1d497b5e5a68ae2f443727b4a609c29930a2
Bruno Marcel <bmnsouza@gmail.com>
2021-10-14 21:16:04 -0300

    Documentação dos métodos

--- base-spring-boot/src/main/java/br/com/boot/spring/base/util/CacheUtil.java
+++ base-spring-boot/src/main/java/br/com/boot/spring/base/util/CacheUtil.java
@@ -6,16 +6,37 @@ import org.springframework.cache.annotation.Cacheable;
 
 public class CacheUtil {
 	
+	/**
+	 * Obtém o valor da key
+	 * @param cdSistema
+	 * @param nmSecao
+	 * @param nmParametro
+	 * @return String
+	 */
 	@Cacheable(value = "cache", key = "{#cdSistema, #nmSecao, #nmParametro}", unless = "#result == null")
 	public String getCache(String cdSistema, String nmSecao, String nmParametro) {
 		return null;
 	}
 	
+	/**
+	 * Cadastra ou atualiza o valor para a key
+	 * @param cdSistema
+	 * @param nmSecao
+	 * @param nmParametro
+	 * @param vlParametro
+	 * @return String
+	 */
 	@CachePut(value = "cache", key = "{#cdSistema, #nmSecao, #nmParametro}", unless = "#vlParametro == null")
 	public String setCache(String cdSistema, String nmSecao, String nmParametro, String vlParametro) {
 		return vlParametro;
 	}
 	
+	/**
+	 * Exclui a key
+	 * @param cdSistema
+	 * @param nmSecao
+	 * @param nmParametro
+	 */
 	@CacheEvict(value = "cache", key = "{#cdSistema, #nmSecao, #nmParametro}")
 	public void delCache(String cdSistema, String nmSecao, String nmParametro) { }
 
