02cbcd6636634be5a26a149854634b38a3916d47
Bruno Marcel <bmnsouza@gmail.com>
2021-10-23 01:47:14 -0300

    Melhorias de métodos na classe de exceção

--- biblioteca-spring-boot/src/main/java/br/com/boot/spring/biblioteca/exception/RestControllerAdviceException.java
+++ biblioteca-spring-boot/src/main/java/br/com/boot/spring/biblioteca/exception/RestControllerAdviceException.java
@@ -2,10 +2,10 @@ package br.com.boot.spring.biblioteca.exception;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import br.com.boot.spring.base.exception.HandleException;
+import br.com.boot.spring.base.exception.ApiExceptionHandler;
 
 /**
- * Herda da classe {@link HandlerException} que possui os tratamentos de exceções para todos os projetos.
+ * Herda da classe {@link ApiExceptionHandler} que possui os tratamentos de exceções para todos os projetos.
  */
 @RestControllerAdvice
-public class RestControllerAdviceException extends HandleException {}
+public class RestControllerAdviceException extends ApiExceptionHandler {}
